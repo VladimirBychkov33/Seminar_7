6f7eb6f5531b0ecbefc12a441c9326501b4ba2b6
Vladimir <grandtwix33@mail.ru>
2022-11-26 01:09:31 +0500

    add Task_52

---
+++ Task_52/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Clear();
+int[,] array = GetArray(3, 3, 1, 9);
+PrintArray(array);
+Console.WriteLine();
+Console.Write($"Сумма элементов первого столбца равна {Average(array)}");
+
+int Average(int[,] massiv)
+{
+    int sum1 = 0;
+    for (int i = 0; i < massiv.GetLength(0); i++)
+    {
+        for (int j = 0; j < massiv.GetLength(1); j++)
+        { 
+            
+            sum1 = massiv[0,0] + massiv[1,0] + massiv[2,0];
+        }
+    }
+    return sum1;
+}
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+}
